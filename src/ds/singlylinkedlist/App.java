8fe34c4ae2ff060398ed0bd77eb5e55369ed2498
Josia <josiarodriguez@gmail.com>
2020-09-20 15:48:13 -0400

    basic singlylink

---
+++ src/ds/singlylinkedlist/App.java
@@ -0,0 +1,14 @@
+package ds.singlylinkedlist;
+
+public class App {
+    public static void main(String[] args){
+        SinglyLinkedList mylist = new SinglyLinkedList();
+        mylist.insertFirst(100);
+        mylist.insertFirst(50);
+        mylist.insertFirst(99);
+        mylist.insertFirst(88);
+        mylist.insertLast(88888888);
+
+        mylist.displayList();
+    }
+}
